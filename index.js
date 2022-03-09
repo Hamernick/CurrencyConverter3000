8b514d1c5f287c0180586835b5f01fdd09b4beb8
Kevawn Warren <kevawnw@gmail.com>
2022-03-09 09:52:33 -0500

    new push

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 fetch('http://apilayer.net/api/live?access_key=e12a8ff2cb281a22f95288d44f4476f4&currencies=EUR,GBP,CAD,AUD,MXN&source=USD&format=1')
 .then(response => response.json())
 .then(data => callback(data))
@@ -20,4 +21,44 @@ function callback(data){
         const returnValue = input.value * dropdown.value
         h1.append(returnValue)
     })
+=======
+fetch('http://apilayer.net/api/live?access_key=e12a8ff2cb281a22f95288d44f4476f4&currencies=EUR,GBP,CAD,AUD,MXN&source=USD&format=1')
+.then(response => response.json())
+.then(data => callback(data))
+
+function callback(data){
+    
+    const rateX = data.quotes
+    console.log(rateX)
+    const dropdown = document.querySelector('select#currency-types')
+    const form = document.querySelector('form')
+    const input = document.querySelector('input#fname')
+    
+    const h1 = document.querySelector('h1#return-currency')
+    
+
+
+
+    Object.entries(rateX).forEach(data => {
+        console.log(data)
+        const options = document.createElement('option')
+        options.textContent = data[0]
+        options.value = data[1]
+        
+        dropdown.append(options)
+    })
+
+    
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const returnValue = input.value * dropdown.value
+        console.log(returnValue)
+
+        
+    })
+    
+
+
+>>>>>>> 7c007d4 (prototype code)
 }
